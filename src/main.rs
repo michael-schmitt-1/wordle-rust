c76b54c05d7222bc02771c4072704b3a42681405
Sebastian Kempf <sebastian.kempf@anevis-solutions.com>
2024-05-29 23:36:03 +0200

    add basic input

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use crossterm::{
     ExecutableCommand,
 };
 use ratatui::{
+    layout::{Constraint, Direction, Layout},
     prelude::{CrosstermBackend, Terminal},
     widgets::Paragraph,
 };
@@ -17,19 +18,35 @@ fn main() -> Result<()> {
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
     terminal.clear()?;
 
+    let mut text_list: String = "".to_string();
+    let mut input: String = "".to_string();
+
     loop {
         terminal.draw(|frame| {
-            let area = frame.size();
-            frame.render_widget(
-                Paragraph::new("Hello Ratatui! (press 'q' to quit)"),
-                area,
-            );
+            let layout = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints(vec![Constraint::Min(6), Constraint::Max(1)])
+                .split(frame.size());
+
+            let _area = frame.size();
+            frame.render_widget(Paragraph::new(text_list.clone()), layout[0]);
+            frame.render_widget(Paragraph::new(input.clone()), layout[1]);
         })?;
 
         if event::poll(std::time::Duration::from_millis(16))? {
             if let event::Event::Key(key) = event::read()? {
                 if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
                     break;
+                }
+
+                match key.code {
+                    KeyCode::Char(c) => input.push(c),
+                    KeyCode::Enter => {
+                        text_list.push_str(&input.clone());
+                        text_list.push('\n');
+                        input = "".to_string();
+                    }
+                    _ => continue,
                 }
             }
         }
